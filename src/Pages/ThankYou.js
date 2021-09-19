d7fd63b2e5bad8eed9e11595bdc07e0439951d7d
anumoitreyee01 <76536964+anumoitreyee01@users.noreply.github.com>
2021-09-19 16:57:11 +0530

    done

--- src/Pages/ThankYou.js
+++ src/Pages/ThankYou.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import ViewMore from '../Components/ViewMoreButton';
-
+import done from './../Assets/done.jpg';
 
 
 
@@ -14,6 +14,7 @@ export default function ThankYou() {
 
             <p> <h2>Thank You! </h2></p>
           
+          <img src={done} alt="done" />
           <p> Your Request has been created.</p>
            <br></br>
            <p>Keep Documents Ready</p>
